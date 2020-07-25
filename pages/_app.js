bb4ed4c1d76937feeac3524e47c957554b4cfedd
juansgaitan <juans.gaitan@gmail.com>
2020-07-25 19:07:23 +0200

    Add gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
+import * as gtag from '../lib/gtag'
+import { useEffect } from 'react'
+import Router from 'next/router'
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
