9fb2cc1f378a0dd07dd06a2ae18f484a31a11660
Juan Sebastián Gaitán Villamizar <juans.gaitan@gmail.com>
2021-04-04 16:29:21 +0200

    Allow X-Phisherman header

--- pages/api/fredirect.js
+++ pages/api/fredirect.js
@@ -151,7 +151,7 @@ const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Phisherman'
   );
   if(req.method === 'OPTIONS') {
     res.status(200).end();
