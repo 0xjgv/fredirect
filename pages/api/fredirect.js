9eb29a6b270cd9880c4358120372c67462ff62e0
Juan Sebastián Gaitán Villamizar <juans.gaitan@gmail.com>
2021-04-04 16:26:51 +0200

    Handle CORS

--- pages/api/fredirect.js
+++ pages/api/fredirect.js
@@ -140,9 +140,27 @@ const startFollowing = async (urlObject) => {
   return {urls, records};
 };
 
-export default async (req, res) => {
+const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Echo user's origin
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  // Allow all
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if(req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+const handler = async (req, res) => {
   try {
     const url = new URL(prefixWithHttp(req.query.url));
     const redirects = await startFollowing(url);
@@ -154,3 +172,5 @@ export default async (req, res) => {
     return res.status(400).send(error);
   }
 };
+
+export default allowCors(handler);
