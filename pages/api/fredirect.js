8879ddc72e2087b42b79e96a3f7c447f14c0632c
Juan Villamizar <juans.gaitan@gmail.com>
2023-06-25 15:10:35 +0200

    Remove origin header

--- pages/api/fredirect.js
+++ pages/api/fredirect.js
@@ -149,21 +149,12 @@ const startFollowing = async urlObject => {
 };
 
 const allowCors = fn => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Echo user's origin
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    req.headers.origin || req.headers.referer
-  );
-  // Allow all
-  // res.setHeader('Access-Control-Allow-Origin', '*');
-
-  res.setHeader("Access-Control-Allow-Methods", "OPTIONS,GET");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Phisherman"
   );
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS,GET");
+  res.setHeader("Access-Control-Allow-Credentials", true);
   if (req.method === "OPTIONS") {
     res.status(200).end();
     return;
